a9f0ed6a35a0332f1e3b32afc34aa2111c068654
wtzhu13 <wtzhu_13@163.com>
2021-03-15 17:29:09 +0800

    add some code to project

---
+++ SpatialFiltering/src/RobertsEdge.m
@@ -0,0 +1,48 @@
+%% --------------------------------
+%% author:wtzhu
+%% email:wtzhu_13@163.com
+%% date: 20210203
+%% fuction: Roberts ½»²æÌÝ¶ÈËã×Ó
+%% --------------------------------
+
+clear;
+clc;
+close all;
+f = imread('./images/house.tif');
+figure();
+subplot(131);imshow(f);title('org');
+% f = [0 0 0 255 255 255;
+%      0 0 0 255 255 255;
+%      0 0 0 255 255 255;
+%      0 0 0 255 255 255;
+%      0 0 0 255 255 255;
+%      0 0 0 255 255 255;];
+I = im2double(f);
+[m, n] = size(f);
+B=zeros(size(f));
+for x=1:m-1
+    for y=1:n-1
+        B(x,y)=abs(I(x, y)+I(x, y+1)-I(x+1, y)-I(x+1, y+1))+abs(I(x, y)+I(x+1, y)-I(x, y+1)-I(x+1, y+1));
+    end
+end
+% B_max = max(max(B));
+% B_min = min(min(B));
+% thresh = 35/255;
+% for x=1:m
+%     for y=1:n
+%         if B(x, y) > ((B_max-B_min) * thresh)
+%             B(x,y)=255;
+%         else
+%             B(x,y)=0;
+%         end
+%     end
+% end
+subplot(132);imshow(B);title('new img');
+
+C=zeros(size(f));
+for x=1:m-1
+    for y=1:n-1
+        C(x,y)=abs(I(x, y)-I(x+1, y+1))+abs(I(x, y+1)-I(x+1, y));
+    end
+end
+subplot(133);imshow(C);title('new img');
--- SpatialFiltering/src/test.m
+++ SpatialFiltering/src/test.m
@@ -1,15 +1,4 @@
-x = -3 :0.02 :10;
-sizeX = size(x);
-y = zeros(1, sizeX(2));
-for i = 1: sizeX(2);
-    if x(i) < 0
-        y(i) = 0;
-    elseif x(i) >= 0 && x(i) <= 6.28
-        y(i) = sin(5*x(i));
-    else
-        y(i) = sin(15*x(i)); 
-    end
-end
-ylim([-2, 2]);
-set(gca,'YTick', -2:0.02:2);
-plot(x, y);      
+x = [-1 -0.8 -0.6 -0.4 -0.2 0 0 0.2 0.4 0.6 0.8 1];
+y = [0 0 0 0 0 0 1 1 1 1 1 1];
+plot(x, y, 'r');  
+ylim([-0.15 1.5]);
